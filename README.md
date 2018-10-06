668674a6a0d361b48cf0f1a5b9d9f47805ea767d
Snegil <144wallfin@gmail.com>
2018-10-06 14:31:34 +0200

    Changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # PHP Project
+Trying to learn PHP
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		
+		<title>
+			PHP-Project
+		</title>
+		
+		<link rel="stylesheet" type="text/css" href="stylesheet.css" />
+	</head>
+	
+	<body>
+		
+		<div id="middlebox">
+		<?php
+			$txt = "test";
+			echo " $test";
+			?>
+		</div>
+		
+	</body>
+</html>
