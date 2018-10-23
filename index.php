2bf51b8527dac15038ff226716c85ab13871ad1c
Snegil <34574057+Snegil@users.noreply.github.com>
2018-10-23 10:51:05 +0200

    Fixed login page that goes to absolutely nothing

--- index.php
+++ index.php
@@ -1,29 +1,55 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		
 		<title>
-			PHP-Project
+			Classic
 		</title>
-		
-		<link rel="stylesheet" type="text/css" href="stylesheet.css" />
+		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 	</head>
-	
 	<body>
-		
-		<div id="middlebox">
-			<div id="topspace">
-		<?php
-			$txt = 5;
+	<div id="leftbox">
+	  <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+		Username: <br>
+		<input type="text" name="name"> <br>
+		Password <br>
+		<input type="password" name="pass"> <br>
+		<br>
+		<input type="submit" name="Submit">
+	</form>
+	</div>
+
+		<div id="mainbox">
 			
+		
+			
+			<?php
+			// define variables and set to empty values
+			$name = $email = "";
+
+			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+			  $name = test_input($_POST["name"]);
+ 			  $pass = test_input($_POST["pass"]);
+			}
+
+			function test_input($data) {
+			  $data = trim($data);
+			  $data = stripslashes($data);
+ 			  $data = htmlspecialchars($data);
+			  return $data;
+			}
 			?>
-			</div>
-			<div id="botspace">
-				<?php 
-				echo "$txt";
-				?>
-			</div>
+			<?php
+  			  $myfile = fopen("loginData.txt", "a+");
+    		  $data = "Login name : ".$_POST['name']." -> Password : ".$_POST['pass'];
+    		  fwrite($myfile, $data);
+    		  fclose($myfile);
+			 ?>
+		
+		<div id="infobox">
+			Name: <?php echo $_POST["name"]; ?> <br>
+			Password: <?php echo $_POST["pass"]; ?>
 		</div>
 		
+		</div>
 	</body>
 </html>
