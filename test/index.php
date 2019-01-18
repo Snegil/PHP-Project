d630f011adaa260bff7a504a771ea727d3d82da8
Snegil <34574057+Snegil@users.noreply.github.com>
2019-01-18 08:41:43 +0100

    Better F logo

--- test/index.php
+++ test/index.php
@@ -21,7 +21,7 @@
 													
 										<div id="infobar">
 											<a target="_blank" href="https://www.facebook.com/Runez-Rock-109810822368941/">
-											<img id="flogo" alt="Runez Rock finns på facebook!" src="Images/flogo.png">
+											<img id="flogo" alt="Runez Rock finns på facebook!" src="Images/flogo2.png">
 											</a>
 											<iframe id="spotifyI" src="https://open.spotify.com/embed/track/40roys5JxjibSvp27c9ts6" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
 										</div>
